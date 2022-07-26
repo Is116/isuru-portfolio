3c48d76c028e401057aa54780673879d5d2be6aa
Isuru Pathirathna <isuru2002@gmail.com>
2022-07-27 00:59:56 +0530

    header ui edits

--- src/components/header/Header.component.jsx
+++ src/components/header/Header.component.jsx
@@ -11,18 +11,18 @@ const Header = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-            <div className="collapse navbar-collapse " id="navbarNav">
+            <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav mx-auto">
-                <li className="nav-item me-5">
+                <li className="nav-item me-lg-5">
                     <a className="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
-                <li className="nav-item me-5">
+                <li className="nav-item me-lg-5">
                     <a className="nav-link active" href="/about">About</a>
                 </li>
-                <li className="nav-item me-5">
+                <li className="nav-item me-lg-5">
                     <a className="nav-link active" href="/contact">Contact</a>
                 </li>
-                <li className="nav-item me-5">
+                <li className="nav-item me-lg-5">
                     <a className="nav-link active" href="/gallery">Gallery</a>
                 </li>
             </ul>
