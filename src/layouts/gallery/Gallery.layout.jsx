80db43c612dde4206e04eb22c518fa13f84ca94d
Isuru Pathirathna <72855496+Is116@users.noreply.github.com>
2023-09-11 13:08:13 +0530

    Update Gallery.layout.jsx

--- src/layouts/gallery/Gallery.layout.jsx
+++ src/layouts/gallery/Gallery.layout.jsx
@@ -6,7 +6,7 @@ const Gallery = () => {
     <div className='col-12 mx-auto mt-5'>
         <h2>GALLERY</h2>
         <hr/>
-        <InstaFeeds token="IGQVJVYm5aOTFtOFgxZAVpuWTZAnUVE5MVZApTTE0ankwWWZA6MTRQNXNYOWVaYXhtVS1hUWtiNW5fNW5VZATZACdElOdWFBMU1VZA1lSRGFWeU5IcWxUS081ZA3gxWm4yMGRUX3V2TkFUa3hIbGhteWR0eUEzagZDZD" limit="12"/>
+        <InstaFeeds token="IGQWRPM0NGeV9NdXV5ZAklvV1hibktNMU9YVDluLThpaGNrbHVSZAkRYUTJXTWtLaVUzTEVicExiU0lxZAl9oUE9FalBramMxZATN5dmtPbDM5VE83TWNCbGhWN21CZAENYallzTW1lRGE4Snl1c0JfS1hMRDI1SGlKWDAZD" limit="12"/>
     </div>
   )
 }
